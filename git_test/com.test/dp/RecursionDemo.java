07b401701a4b6f2c63ebb608d6e435e0367d147b
zhangyongping <zhangyongping@tezign.com>
2021-01-28 17:48:12 +0800

    幂次方

--- git_test/com.test/dp/RecursionDemo.java
+++ git_test/com.test/dp/RecursionDemo.java
@@ -1,11 +1,13 @@
 package dp;
 
 import java.math.BigDecimal;
+import java.util.*;
 
 public class RecursionDemo {
 
     public static void main(String[] args) {
-        System.out.println(fib(4));
+        // System.out.println(fib(4));
+        System.out.println(myPow(2, -3));
     }
 
     // leetcode 斐波那契 结果取余
@@ -21,5 +23,82 @@ public class RecursionDemo {
             m = r;
         }
         return r;
+    }
+
+    public int[] divingBoard(int shorter, int longer, int k) {
+        if (k == 0){
+            return new int[0];
+        }
+        if (shorter == longer){
+            return new int[]{shorter * k};
+        }
+        int[] len = new int[k + 1];
+        for (int i = 0; i <= k; i++){
+            len[i] = i * shorter + (k - i) * longer;
+        }
+        return len;
+    }
+
+    private void divingBoard2(int shorter, int longer, int k, List<Integer> list){
+        if (k == 0){
+            return;
+        }
+        if (k == 1){
+
+        }
+    }
+
+
+    public List<String> letterCombinations(String digits) {
+        String[][] dict = {
+                {"a", "b", "c"},
+                {"d", "e", "f"},
+                {"g", "h", "i"},
+                {"j", "k", "l"},
+                {"m", "n", "o"},
+                {"p", "q", "r", "s"},
+                {"t", "u", "v"},
+                {"w", "x", "y", "z"}
+        };
+        String[][] digitArr = new String[digits.length()][];
+        for (int i = 0; i < digits.length(); i++){
+            digitArr[i] = dict[(int)digits.charAt(i) - 2];
+        }
+        return null;
+    }
+
+    public void letterCombinations(String[][] strs, List<String> res, int idx) {
+        String[] idxArr = strs[idx];
+
+
+    }
+    // x 的 幂次方  位运算
+    public static double myPow(double x, int n) {
+        if(x == 0) return 0;
+        long b = n;
+        double res = 1.0;
+        if(b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        while(b > 0) {
+            if((b & 1) == 1) res *= x;
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+
+
+    public static double myPow3(double x, int n, double[] r) {
+        if (r[n] != 0){
+            return r[n];
+        }
+        if (n % 2 == 0){
+            r[n] = myPow3(x, n / 2, r) * myPow3(x, n / 2, r);
+        } else {
+            r[n] = myPow3(x, n / 2, r) * myPow3(x, n / 2, r) * x;
+        }
+        return r[n];
     }
 }
